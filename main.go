b841de7ed361f77457c45f901972f92ed9536efb
Alberto Franco <afranco_dev@outlook.com>
2024-12-02 00:13:36 -0500

    add log when server starts

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/afranco07/receipt-processor/database"
 	"github.com/afranco07/receipt-processor/handler"
-	"net/http"
 )
 
 func main() {
@@ -13,6 +15,7 @@ func main() {
 	http.HandleFunc("GET /receipts/{id}/points", receiptHandler.GetPointsForID)
 	http.HandleFunc("POST /receipts/process", receiptHandler.ProcessReceipt)
 
+	log.Println("Starting server on port :8080…")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
